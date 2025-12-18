c3cde69c7535b9b951b78c8c24da85fb8d3e0919
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-18 06:26:16 +0100

    format

--- Lox/Program.cs
+++ Lox/Program.cs
@@ -9,7 +9,7 @@ static class Program
             Console.Error.WriteLine($"Usage: Lox [script]");
             return 1;
         }
-        
+
         if (args.Length == 1)
         {
             RunFile(args[0]);
@@ -39,7 +39,7 @@ static class Program
             {
                 return;
             }
-            
+
             Run(line);
         }
     }
