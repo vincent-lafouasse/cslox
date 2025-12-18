1f65c1e290b81fa3d6e6252355568d87a5867cc3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-18 08:02:27 +0100

    basic Token class

--- Lox/Lox.cs
+++ Lox/Lox.cs
@@ -45,6 +45,22 @@ public enum TokenType
 	Eof,
 }
 
+class Token
+{
+	readonly TokenType type;
+	readonly string lexeme;
+	readonly object? literal;
+	readonly int line;
+
+	Token(TokenType type, string lexeme, object? literal, int line)
+	{
+		this.type = type;
+		this.lexeme = lexeme;
+		this.literal = literal;
+		this.line = line;
+	}
+};
+
 internal static class ExitCodes
 {
 	public const int Success = 0;
